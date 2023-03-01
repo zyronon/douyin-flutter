c5432bc42154b0e60639f42d3765543c0d7be9ac
wangnianchao <wangnianchao@lerjin.com>
2023-03-01 11:46:33 +0800

    save

---
+++ lib/pages/home.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+export '';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(18.0),
+          decoration: const BoxDecoration(
+            color: Color.fromRGBO(15, 22, 33, 1),
+          ),
+          child: Column(
+            children: [
+              const Text(
+                '红袖阁',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Column(
+                        children: [
+                          // const Image(
+                          //     image: AssetImage("images/like-red-small.png"),
+                          //     width: 100.0),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '手机号',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 10.0),
+                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            decoration: BoxDecoration(
+                              color: const Color.fromRGBO(24, 32, 46, 1),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  style: const TextStyle(color: Colors.white),
+                                  decoration: const InputDecoration(
+                                    hintText: 'Enter your email',
+                                    hintStyle: TextStyle(
+                                        color: Color.fromRGBO(64, 69, 82, 1)),
+                                    border: InputBorder.none,
+                                  ),
+                                  validator: (String? value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter some text';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '验证码',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 10.0),
+                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            decoration: BoxDecoration(
+                              color: const Color.fromRGBO(24, 32, 46, 1),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: TextFormField(
+                                    style: const TextStyle(color: Colors.white),
+                                    decoration: const InputDecoration(
+                                      hintText: 'Enter your email',
+                                      hintStyle: TextStyle(
+                                          color: Color.fromRGBO(64, 69, 82, 1)),
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
+                                const Text(
+                                  '验证码',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('123');
+                          //导航到新路由
+                          // Navigator.push(
+                          //   context,
+                          //   CupertinoPageRoute(
+                          //     builder: (context) => NewRoute(),
+                          //   )
+                          // );
+                          Navigator.pushNamed(context, 'login');
+                          // Validate will return true if the form is valid, or false if
+                          // the form is invalid.
+                        },
+                        child: const Text('Submit'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text("data")
+            ],
+          )),
+    );
+  }
+}
